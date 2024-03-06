066debf15394219d99533020db8929884cb89536
Pereira-Luc <71381029+Pereira-Luc@users.noreply.github.com>
2024-03-06 15:37:37 +0100

    Add Latex support

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -3,6 +3,29 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
+-- lazy.nvim
+  {
+    "lervag/vimtex",
+    lazy = false,
+    init = function()
+      -- Use init for configuration, don't use the more common "config".
+      --vim.g['vimtex_view_method'] = 'zathura'
+      vim.g['vimtex_view_method'] = 'zathura_simple'
+      vim.g['vimtex_quickfix_mode'] = 0
+      vim.g['vimtex_mappings_enabled'] = 0
+      vim.g['vimtex_indent_enabled'] = 0             -- Auto Indent
+      vim.g['tex_flavor'] = 'latex'                  -- how to read tex files
+      vim.g['tex_indent_items'] = 0                  -- turn off enumerate indent
+      vim.g['tex_indent_brace'] = 0                  -- turn off brace indent
+      vim.g['vimtex_context_pdf_viewer'] = 'okular'  -- external PDF viewer run from vimtex menu command
+      vim.g['vimtex_log_ignore'] = ({                -- Error suppression:
+                                      'Underfull',
+                                      'Overfull',
+                                      'specifier changed to',
+                                      'Token not allowed in a PDF string',
+    }) 
+    end
+  },
   -- Override plugin definition options
   {
     "christoomey/vim-tmux-navigator",
